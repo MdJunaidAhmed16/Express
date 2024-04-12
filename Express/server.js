31d9f5c67f113c8437c77df98d1a2d3f1abea5bf
Mohammed Junaid Ahmed <junaid-17525@junaid-17525.csez.zohocorpin.com>
2024-04-13 03:06:47 +0530

    Middleware for running static files

--- Express/server.js
+++ Express/server.js
@@ -7,6 +7,9 @@ app.set('view engine', 'ejs');
 // everything goes in order if it would use below default path then the default path get would not be logged
 app.use(logger);
 
+// This Middleware is  to render the static files directly with the URI
+app.use(express.static("public"));
+
 // as I have declare over here the logger it is called as Middleware
 // we can have n number of Middlewares this way
 app.get('/', logger, (req, res) => {
