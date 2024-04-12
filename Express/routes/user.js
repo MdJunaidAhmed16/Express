04c6d75f5c7c6513f75f72f2b576bd3a83158182
Mohammed Junaid Ahmed <junaid-17525@junaid-17525.csez.zohocorpin.com>
2024-04-13 02:47:01 +0530

    Routes learnt

--- Express/routes/user.js
+++ Express/routes/user.js
@@ -4,18 +4,42 @@ const router = express.Router();
 
 // finding the routes goes in order
 
-router.get('/', (req, res) => {
-    res.send("Redirected to users page");
-});
+// router.get('/', (req, res) => {
+//     res.send("Redirected to users page");
+// });
 
-router.get('/new', (req, res) => {
-    res.send("Redirected to users new add page");
-});
+// router.get('/new', (req, res) => {
+//     res.send("Redirected to users new add page");
+// });
 
-// dynamically fetching with the routes
-router.get('/:id', (req, res) => {
+// // dynamically fetching with the routes
+// router.get('/:id', (req, res) => {
+//     let userId = req.params.id;
+//     res.send(`Get with user id : ${userId}`);
+// });
+
+// as for the CRUD on particular URL every time we need to check :id like this to avoid this
+
+router.route('/:id').get((req, res) => {
     let userId = req.params.id;
     res.send(`Get with user id : ${userId}`);
+    console.log(req.user);
+}).put((req, res) => {
+    let userId = req.params.id;
+    res.send(`Put : ${userId}`);
+}).delete((req, res) => {
+    let userId = req.params.id;
+    res.send(`Deleting user id : ${userId}`);
+});
+
+const users = [{name : "Junaid"}, {name : "Abid"}];
+
+
+// This is to do some action once the request has loaded the specified URI and do some action such as setting
+// user in req and then printing the user from req
+router.param("id", (req, res, next, id) => {
+    req.user = users[id];
+    next();
 });
 
 
